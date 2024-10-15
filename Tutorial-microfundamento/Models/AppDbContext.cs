43c2c046364199f7e88d9536aa2aad56fc591707
Laura <laurafurtadoamaral@gmail.com>
2024-10-15 16:53:39 -0300

    Configuração do Banco de Dados e da Tabela Veiculo

---
+++ Tutorial-microfundamento/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tutorial_microfundamento.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Veiculo> Veiculos { get; set; }
+    }
+}
---
+++ Tutorial-microfundamento/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tutorial_microfundamento.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Obrigatório informar nome!")]
+        public string Nome { get; set; }
+        
+        [Required (ErrorMessage = "Obrigatório informar placa!")]
+        public string Placa  { get; set; }
+        
+        [Required(ErrorMessage = "Obrigatório informar Ano de fabricação!")]
+        public int AnoFabricacao { get; set; }
+        
+        [Required(ErrorMessage = "Obrigatório informar Ano do modelo!")]
+        public int AnoModelo { get; set; }
+    }
+}
