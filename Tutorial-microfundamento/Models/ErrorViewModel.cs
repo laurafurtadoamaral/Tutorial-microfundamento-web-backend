e5e8f7d2171decd328931658d3d04efe9aa3c25c
Laura <laurafurtadoamaral@gmail.com>
2024-10-15 14:39:24 -0300

    Adicionar arquivos de projeto.

---
+++ Tutorial-microfundamento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tutorial_microfundamento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
